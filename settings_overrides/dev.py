cf94a6839054b90e251f835b58cdce0fe74cb4cb
glards <sglardon@gmail.com>
2013-10-02 00:03:16 +0200

    Delete dev.py

--- settings_overrides/dev.py
+++
@@ -1,7 +0,0 @@
-
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'zoink'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'dks31rzu'         # Not used with sqlite3.
-DATABASE_HOST = '192.168.1.41'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
